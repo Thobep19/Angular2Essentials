0359e68ebfb6e5daeb1d91b82a0ce726b7c06d9e
Thobep19 <thobep19@mail.fresnostate.edu>
2017-04-04 22:16:39 -0700

    Section 02_06b completed

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component ({
     selector: 'mw-app',
     templateUrl: 'app/app.component.html'
+    stylesUrls: ['app/app.component.css']
 })
 export class AppComponent {}
